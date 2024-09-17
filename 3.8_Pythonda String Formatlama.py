c99a0488b5bfcaf614d8b6e2e7280d9690cd306e
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-17 17:19:10 +0300

    ders 9

--- 3.8_Pythonda String Formatlama.py
+++ 3.8_Pythonda String Formatlama.py
@@ -1,1 +1,20 @@
-s
+name = "Furkan"
+surname = "Gül"
+
+print('My name is {} {}' .format(name,surname))
+"""
+Bu format ise bi önceki derste sıra ile yazmak yerine daha pratik bi şekilde değikenleri çıktı olarak çıkarmamıza yardım ediyor 
+"""
+print('My name is {1} {0}' .format(name,surname))
+"""
+Şeklindeki örnekte ise my name is gül furkan şeklinde olur çünkü sayıya duyarıldır.
+"""
+
+
+result = 200/500
+
+print("the result is {r:5.4}".format(r=result))
+""" 
+bu örnekte resut değerini r olarak atadım r karşısındaki 5 virgülden önce ne kadar alan bırakılması gerektiğini yazmamızı sağlıyor
+fakat 4 ise virgülden sonraki 4 haneyi göstermesi gerektiğini ifade ediyor
+"""

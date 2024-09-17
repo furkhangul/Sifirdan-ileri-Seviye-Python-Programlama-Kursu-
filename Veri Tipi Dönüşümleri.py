8c5ca87ef9cd9f289a2916b2e041945d102e8837
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-17 16:02:33 +0300

    ders 7

---
+++ Veri Tipi Dönüşümleri.py
@@ -0,0 +1,17 @@
+'''
+Bu derste örnek yaptım. İşte örnekşeler:
+
+Daire : pi * r * r
+Daire Çevresi : pi * r * 2
+
+*Yarıçapı verilen dairenin alanını ve çevresini bulunuz: 
+'''
+
+yaricap = input("Yarı çap: ")
+yaricap = float(yaricap)
+pi = 3.14
+alan = pi * (yaricap ** 2)
+cevre = pi * yaricap * 2
+
+print("Dairenin Alanı:",alan)
+print("Dairenin Çevresi:",cevre)

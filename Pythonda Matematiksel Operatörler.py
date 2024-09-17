4c63ca5001dc441f41597a1c41615fc5416cabc9
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-17 11:45:44 +0300

    ders

---
+++ Pythonda Matematiksel Operatörler.py
@@ -0,0 +1,40 @@
+#Toplama : +
+#Çıkarma : -
+#Çarpma  : *
+#Bölme   : /
+#Üssü    : **
+#Mod     : %
+#Tam böl : //
+
+#Cmd koumutu veya powershell üzerine python yazdıktan sonra gerekli hesaplamaalrı yapabiliriz
+
+print(2+2)
+#cevap : 4
+print(2**5)
+#cevap : 32
+print(4-.0.5)
+#cevap :3.5
+print(0.5*2)
+#cevap :1.0
+print(1/2)
+#cevap :0.5
+print(20/2.0)
+#cevap :10.0
+print(3**3)
+#cevap :27
+print(10%2)
+#cevap :0
+print(11%2)
+#cevap :1
+print(15456212%5)
+#cevap :2
+print(10/3)
+#cevap :3.3333333
+print(15//4)
+#cevap :3
+print(-15//4)
+#cevap :-4
+print(10//3)
+#cevap :3
+print(-10//3)
+#cevap :-4

f2977ec386d6195381c7c0776b293cfd7a828f14
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-19 19:07:53 +0300

    ders 21

---
+++ 4.1_Atama Operatörleri.py
@@ -0,0 +1,26 @@
+"""
+x = 5
+y = 10
+z = 20
+şeklinde bi yazım yerine daha pratik yazılabilir.
+"""
+x,y,z = 5,10,20
+
+"""
+x = x+5 gibi bi ifade yerine
+x += 5 ifadesini
+x = x - 5 ifadesi yerine
+x -=5 ifadesini
+x = x * 5 ifadesi yerine
+x *=5 ifadesini
+x = x / 5 ifadesi yerine
+x /=5 ifadesini
+x = x % 5 ifadesi yerine
+x %=5 ifadesini
+x = x // 5 idadesi yerine
+x//=5 ifadesini
+x = x ** 5 ifadesi yerine
+x **=5 ifadesini kullanmak partiklik açısından bizim için daha faydalıdır.
+"""
+
+

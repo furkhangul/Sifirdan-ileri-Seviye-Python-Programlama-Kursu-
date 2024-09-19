d5cda5110436ec5d0d335efe52ae06c592d9730c
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-19 18:44:07 +0300

    ders 21

---
+++ 3.20_Pythonda Value ve Referans Veri Tipi.py
@@ -0,0 +1,20 @@
+x = 5
+y = 50
+"""
+diye iki değişkenimiz olsun 
+"""
+
+x = y
+"""
+şeklinde yazdığımızda ise x değişkeni artık 50 olur.
+"""
+print(x)
+
+#--------------------------------------------------
+a = ["apple","banana"]
+b = ["apple","banana"]
+
+a = b
+b[0] = "grape"
+
+print(a)

d0a3cf1bf50c74740b2a68f2af447f0f57338579
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-22 16:18:13 +0300

    ders 35

---
+++ 6.4_Uygulama - While Döngüleri.py
@@ -0,0 +1,33 @@
+"""
+Bu derste while döngüsünü kullanıyoruz.
+"""
+"""
+Elimizde liste var ise ve bu liste ile işlem yapacaksak for döngüsü
+en iyi çözüm yoluyodu. Fakat elimizde liste yoksa ve listesiz bir 
+şekilde hareket etmek zorunda isek while döngüsünü kullanmak en iyi 
+çözüm yolu olabilir.
+"""
+
+x=0
+while x<100:
+    print(x)
+    x += 1
+
+print("Bitti")
+
+"""
+Veya 
+koşul ifadeleri ile de kurabiliriz.
+"""
+
+a = 1
+while a<=100:
+    if (a % 2 == 0):
+        print(a)
+a +=1
+"""
+Veya giriş ekranı yapabiliriz
+"""
+name = "" #false
+while not name:
+    name = input("İsminizi giriniz:")

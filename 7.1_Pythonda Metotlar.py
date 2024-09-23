ced6ce2dbb9f9e5d976f7214024cda2369026427
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-23 15:09:03 +0300

    ders 42

---
+++ 7.1_Pythonda Metotlar.py
@@ -0,0 +1,18 @@
+"""
+Methodlar: diğer dillerde fonksiyon olarak da kullanılmaktadır.
+"""
+
+list = [1,2,3]
+list.append(4)
+list.pop()
+print(type(list))
+print(list)
+myString = "Hello world"
+myString.upper()
+
+print(type(myString))
+print(myString)
+
+# Yukarıda görülen örnekler geliştirdiğimiz fonksiyonlar ile bir sonraki
+# derslerde komutların çağırılması ile parametreler çalışır ve kısa sürede
+# aynı işlemler terkarlanmadan halledilir.

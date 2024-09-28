5c30c5dbca6c22349760e8bb734f48b43f68f3e2
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-28 20:39:41 +0300

    ders 48

---
+++ 8.1_Nesne Tabanlı Programlama Nedir?.py
@@ -0,0 +1,15 @@
+#Bu derste object orianted programming yani nesne tabalı programlama üzerine
+#çalıştım. bunun için ilk olarak class ifadelerine bakacak olursak:
+
+
+#class
+lst = [1,2,3]
+lst2 =[1,2,3,4]
+result= type(lst)
+print(result)
+
+"""
+Burada iki tane listenin tipi de list türünde olduğunu görüyoruz.
+ yani bu listelerin aslında daha önceden tanımlanmış bir class sınıfından
+ liste sınıfını çağırıyoruz
+ """

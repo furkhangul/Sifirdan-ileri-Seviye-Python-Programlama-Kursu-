ccc36d8b044e70629f0c484283e422e2c808e294
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-09-30 09:06:07 +0300

    ders 53

---
+++ 9.1_Modül Nedir?.py
@@ -0,0 +1,27 @@
+"""
+Bu derste modüller üzerinde çalışıldı.
+"""
+"""
+Modül  yazılım geliştirme sürecinde kodu küçük, bağımsız parçalara ayırma 
+yöntemidir. Açıkça içerisinde fonksiyonları, sınıfları veya değişkenleri
+barındıran bir Python dosyasıdır. Biliyorsunuz fonksiyonları kullanmamızın 
+sebebi sürekli aynı kodları tekrar etmemekti. Bir kere istediğimiz 
+fonksiyonu yazıyoruz ve projemizin herhangi bir yerinde bu fonksiyonu 
+kullanıyoruz. 
+"""
+
+"""
+a modülü + b modülü + c modülü + ana modül = program 
+a.py + b.py + c.py + main.py = prgram
+"""
+
+"""
+Modüller için : 
+1- Kendi ürettiğmiz modüller
+2- Hazır modüller
+  A)Standart kütüphane modülleri
+  B)Üçüncü şahıs modülleri
+  
+            >pip install packet-name.py 
+            şeklinde indirebiliriz.
+"""

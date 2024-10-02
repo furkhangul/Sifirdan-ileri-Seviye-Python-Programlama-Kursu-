c19d4559575e38704f4a717779ed75554e6e6c23
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-10-02 15:57:17 +0300

    der 59

---
+++ 10.1_Hata ve Hata Yönetimi Nedir?.py
@@ -0,0 +1,26 @@
+"""
+Bu derste hata ayıklama ve hata yöneyimi üzerinde çalışıldı.
+"""
+
+"""
+Error = hata 
+Error handing => hata yönetimi 
+"""
+"""
+Hata yönetimini en verimili şekilde kullanmamız için öncelikle hataların 
+çıkaibleceği yerleri iyi bir  şekilde tahmin etmemiz gerekiyor
+"""
+
+"""
+Bunun için hataların olabileceği yerleri işaretliyoruz. 
+Örneğin int("1a2") şeklinde yazıldığı zaman hem intager hem de string
+ifadeler tek yerde kullanılığı için hata verecektir. 
+"""
+
+"""
+docs.python.org sitesi ile hataların olabailecek hata çözümlemelirini
+görebiliriz.
+
+diğer çeşitlerini öğrnemek için bu siteye gitmem lazım 
+"""
+

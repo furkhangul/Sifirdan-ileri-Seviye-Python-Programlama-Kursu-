5547068631d32293d0a54fbbff41f35472a84ef6
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-11-22 15:55:11 +0300

    ders 7,

---
+++ 12.1_Pythonda İç içe Fonksiyon Kullanım.py
@@ -0,0 +1,53 @@
+"""
+Bu derste fonkisyonlarda iç içe fonkisyonları değerlendirdim.
+fonkisyonlar da bi obje olduğunu öğrenerek başlıyoruz.
+"""
+from contextlib import nullcontext
+
+
+def greeting(name):
+    print("Hello "+name )
+
+print(greeting("Furkan"))
+print(greeting)
+
+"""
+Şeklinde yazdığımız zaman furkan kelimesini import ettik
+"""
+
+sayhello = greeting
+print(sayhello)
+"""
+ SAYHELLO silinirse 
+"""
+del sayhello
+
+print(greeting)
+### yeni bi örnek üzerinde duracak olursak :)
+"""
+Bu örnekte fonksiyon içerisine fonksiyon yerleştirebiliyoruz.
+"""
+def outer(num1):
+    def inner_increment(num1):
+        return num1  + 1
+    inner_increment(num1)
+
+
+outer(10)
+
+
+
+### yeni bi örnek yapacak olursak
+
+def factorial(number):
+    def inner_factorial(number):
+        if number <= 1:
+            return 1
+
+        return number * inner_factorial(number  - 1)
+
+    return inner_factorial(number)
+
+print(factorial(4))
+
+

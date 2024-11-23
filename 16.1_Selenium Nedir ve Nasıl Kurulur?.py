8d365e8349ecb7d6412b782d50e4a6f64c0975f5
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2024-11-23 17:27:52 +0300

    ders 81

---
+++ 16.1_Selenium Nedir ve Nasıl Kurulur?.py
@@ -0,0 +1,44 @@
+"""
+Selenium nedir nasıl kullanılır adlı bölümdür.
+"""
+
+"""
+Selenium bir web otomasyon kütüphanesidir.
+selenium ile bir web sitesini ziyaret edip etkileşimde bulunabiliriz.
+"""
+
+"""
+Örneğin rutin bir şekilde paylaşım yapan bir kişi selenium kütüphanesi yardımı
+ile bot yapabilir ve bu bot sayesinde zamandan tasarruf edebilir.
+"""
+
+"""
+selenium kütüphanesi pip install selenium adlı komut ile bilgisayarımıza kurulamktadır.
+cmd komut istemcisini veya ide üzerinden açtığımız konsol üzerinden indirebiliriz.
+"""
+
+"""
+Dediğimiz üzere selenium bir web otomasyon olduğundan web üzerinden ilerlemektedir
+web sitelere ve adreslere erişebilmek için tarayıcılara ihtiyaç duymaktayız.
+bunun için her tarayıcının kendine özgü yardımcı driverları bulunmaktadır.
+Kullandığımız tarayıcıya özgü driveri yükledikten sonra işlem yapabilmekteyiz.
+Kullandığımız tarayıcıya özgü driverlerı indirmek için:
+https://selenium-python.readthedocs.io/installation.html
+sitesi üzerinden ulaşabiliriz.
+
+"""
+
+
+"""
+Örnek olarak chrome tarayıcısı üzerinden bir driver indirecek olursak.
+öncelikle  https://sites.google.com/chromium.org/driver/downloads?authuser=0
+linkine ulaşıyoruz ardından yeni bir sekme açıp bu sekemden üç nokta butonun basıp
+yardım > chrome hakkında kısmına bakıp chrome tarayıcımızın sürümüne bakıyoruz
+sürüm numarasına bakıp az önce girdiğimiz web site üzerinden indirme işlemini yapıyoruz.
+"""
+
+
+"""
+Ardından ise indirdiğimiz driverı kullanacağımız proje ile aynı dizine atmamız 
+gerekemektedir. Veya uzantısını kod içerisinde belirtmemiz gerekmektedir.
+"""
